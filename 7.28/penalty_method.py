bad59a1dccbb5c6e0f6a6f0255fc18bfb50dc96e
Zhenyu Hong <125761006+zhjjhiafj@users.noreply.github.com>
2024-07-31 16:28:37 -0400

    Update penalty_method.py

--- 7.28/penalty_method.py
+++ 7.28/penalty_method.py
@@ -1,8 +1,5 @@
-#test for penalty method 
-
+#penalty method that ensures the solution to be non-negative
 import ufl
-import dolfinx
-import math
 import numpy
 import matplotlib.pyplot as plt
 from mpi4py import MPI
@@ -11,92 +8,82 @@ from dolfinx import mesh, fem, io, nls, log
 from dolfinx.fem.petsc import NonlinearProblem
 from dolfinx.nls.petsc import NewtonSolver
 import numpy as np
-n=2
+n=3
 num_intervals = 1000
-L=1.0
-R=0.75
-start_point = -L
-end_point = L
+start_point = -1.0
+end_point = 1.0
 domain = mesh.create_interval(MPI.COMM_WORLD, num_intervals, [start_point, end_point])
 x = ufl.SpatialCoordinate(domain)
 V = fem.functionspace(domain, ("Lagrange", 1))
 
-b_0 = 0.01
-z_0 = 1.2
+# u_ufl = (2**n*(n+2))**(1.0/(2*n+2))*(1-((x[0]**2)**1/2)**(1.0+1.0/n))**(n*1.0/(2*n+2))
 
 
-# Define the expression for b(x)
-b = b_0 * ufl.cos(z_0 * ufl.pi * abs(x[0]) / R)
+# def u_exact(x):
+#    return eval(str(u_ufl))
+
+
 from dolfinx import default_scalar_type
-#b=fem.Constant(domain, default_scalar_type(0.0))
-u_0 = 1.0 - n /(n-1.0) * (
-    (abs(x[0])/ R)**((n + 1.0) / (n ))
-    - (1.0 - abs(x[0])/ R)**((n + 1.0) / (n ))
-    + 1
-    - (n + 1.0) / (n ) * (abs(x[0]) / R)
-)
-u=ufl.conditional(abs(x[0]) < R, u_0, 0)
-h=u**(n/(2*n+2.0))
-H=h + b
-from dolfinx import default_scalar_type
-q_plus=- ( 1/ (n + 2.0)) *H**(n + 2.0) * ufl.sqrt(ufl.dot(ufl.grad(h), ufl.grad(h)))\
-                            **(n - 1.0) * ufl.grad(h)
-q=ufl.conditional(abs(x[0]) < R, q_plus, ufl.as_vector([0.0] * len(q_plus)))
-f=ufl.div(q)
+num=1/((((n/(2.0*n+2.0))**n) / (n + 2.0)))
+def f1(x):
+    values = np.full_like(x[0], -3.0)
+    mask = (x[0] > -0.5) & (x[0] < 0.5)
+    values[mask] = 1.0
+    return values
+#f = fem.Constant(domain, default_scalar_type(num))
+f=fem.Function(V)
+f.interpolate(f1)
+
 u_D = fem.Function(V)
 u_D.interpolate(lambda x: np.zeros_like(x[0]))
 fdim = domain.topology.dim - 1
 boundary_facets = mesh.locate_entities_boundary(domain, fdim, lambda x: numpy.full(x.shape[1], True, dtype=bool))
 bc = fem.dirichletbc(u_D, fem.locate_dofs_topological(V, fdim, boundary_facets))
-Phi_ufl = -(2 * n + 2.0) / n * pow(u, ((n + 2) / (2 * n + 2.0))) * (ufl.grad(b))
-alpha_plus=(-((n/(2.0*n+2.0))**n) / (n + 2.0)*
-       ufl.div((ufl.dot(ufl.grad(u) - Phi_ufl, ufl.grad(u) - Phi_ufl)) ** ((n - 1.0) / 2.0)\
-           * (ufl.grad(u) - Phi_ufl)))
-alpha2=fem.Function(V)
-alpha3=fem.Expression(alpha_plus,V.element.interpolation_points())
-alpha2.interpolate(alpha3)
-alpha=ufl.conditional(abs(x[0]) < R, alpha_plus, alpha2.x.array[math.ceil(((L-R)/L/2.0)*num_intervals)+1])
-error = float('inf')  # Use infinity as an initial error to ensure the loop starts
-tolerance = 1e-6 # Define a tolerance level
+
+error_L2 = float('inf')  # Use infinity as an initial error to ensure the loop starts
+tolerance = 1e-5  # Define a tolerance level
 max_iterations = 10 # Optional: to prevent infinite loops
 iteration = 0
 uh = fem.Function(V)
-u2=fem.Expression(u,V.element.interpolation_points())
-uh.interpolate(lambda x:0.75-x[0]**2)
 C=fem.Function(V)
 constant_value = 1.0
 C.interpolate(lambda x: np.full_like(x[0], constant_value))
-epsilon=0.000000000001
-while iteration < max_iterations and error > tolerance:
+#b=1-x[0]**2
+b=fem.Constant(domain, default_scalar_type(0.0))
+epsilon=0.00000001
+#b=1-x[0]**2
+
+uh.interpolate(lambda x:1-2*x[0]**2 )
+while error_L2 > tolerance and iteration < max_iterations:
     uh2 = fem.Function(V)
-    uh2.interpolate(lambda x: 1- x[0] ** 2)
+    uh2.interpolate(lambda x:1-x[0]**2)
     v = ufl.TestFunction(V)
     Phi = -(2 * n + 2.0) / n * pow(uh, ((n + 2) / (2 * n + 2.0))) * (ufl.grad(b))
-    a = ((n/(2.0*n+2.0))**n) / (n + 2.0)*(ufl.dot(ufl.grad(uh2) - Phi, ufl.grad(uh2) - Phi)) ** ((n - 1.0) / 2.0)
+    a = (ufl.dot(ufl.grad(uh2) - Phi, ufl.grad(uh2) - Phi)) ** ((n - 1.0) / 2.0)
     # a = pow(ufl.dot(ufl.grad(uh2) - (2 * n + 2.0) / n * pow(uh, ((n + 2) / (2 * n + 2.0))) * ufl.grad(b)\
     # , ufl.grad(uh2)
     #                - (2 * n + 2.0) / n * pow(uh, ((n + 2) / (2 * n + 2.0))) * ufl.grad(b)), (n - 1.0) / 2.0)
-    penalty = uh2 - (ufl.dot(uh2, uh2)) ** 0.5 * C
-
-    F = (a*ufl.dot(ufl.grad(uh2)-Phi, ufl.grad(v))-(alpha-1/epsilon*penalty)*v)*ufl.dx
-
+    k = ufl.div(a*Phi)
+    penalty=uh2-(ufl.dot(uh2,uh2))**0.5*C
+    #F = (a*ufl.dot(ufl.grad(uh2), ufl.grad(v))+k*v-(f)*v)*ufl.dx
+    F = (a * ufl.dot(ufl.grad(uh2)-Phi, ufl.grad(v)) - (f)* (v)) * ufl.dx
+    #F = (a * ufl.dot(ufl.grad(uh2) - Phi, ufl.grad(v)) -f* v) * ufl.dx
     problem = NonlinearProblem(F, uh2, bcs=[bc])
     solver = NewtonSolver(MPI.COMM_WORLD, problem)
     solver.convergence_criterion = "incremental"
-    solver.rtol = 1e-6
+    solver.rtol = 1e-5
+    solver.report = True
+    solver.max_it=2000
     ksp = solver.krylov_solver
-    log.set_log_level(log.LogLevel.INFO)
     opts = PETSc.Options()
     option_prefix = ksp.getOptionsPrefix()
     opts[f"{option_prefix}ksp_type"] = "cg"
     opts[f"{option_prefix}pc_type"] = "ilu"
-    solver.max_it = 1000
     #opts[f"{option_prefix}pc_type"] = "gamg"
     #opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
     ksp.setFromOptions()
 
     log.set_log_level(log.LogLevel.INFO)
-    print("before")
     m, converged = solver.solve(uh2)
     assert (converged)
     print(f"Number of interations: {m:d}")
@@ -106,35 +93,37 @@ while iteration < max_iterations and error > tolerance:
     error_L2 = numpy.sqrt(domain.comm.allreduce(error_local, op=MPI.SUM))
 
     print(f"Iteration: {iteration}, Error: {error_L2}")
+    uh=uh2
     # Increment the iteration counter
-    error=error_L2
     iteration += 1
-    uh.interpolate(uh2)
 
-k=((abs(x[0])/R)**(1.0/n)+(1.0-abs(x[0])/R)**(1.0/n)-1.0)**n
 # Compute L2 error and error at nodes
-V_ex = fem.functionspace(domain, ("Lagrange", 1))
-f1=fem.Expression(u,V_ex.element.interpolation_points())
-f2=fem.Function(V_ex)
+V_ex = fem.functionspace(domain, ("Lagrange", 2))
+u_ex = fem.Function(V_ex)
+# u_ex.interpolate(u_exact)
+#u_values = uh.x.array**(n/(2*n+2.0))
+f1=fem.Expression(f,V.element.interpolation_points())
+f2=fem.Function(V)
 f2.interpolate(f1)
-u_values=f2.x.array
-u_values2 = uh.x.array
-x_values = np.linspace(start_point, end_point, num_intervals+1 )
-b1=fem.Expression(b,V_ex.element.interpolation_points())
-b2=fem.Function(V_ex)
-b2.interpolate(b1)
-b_values=b2.x.array
+u_values2=f2.x.array
+u_values = uh.x.array
+#u_values = abs(uh.x.array)**(n/(2*n+2.0))
+x_values = np.linspace(start_point, end_point, num_intervals + 1)
 
 if domain.comm.rank == 0:
     plt.show()
     plt.figure()
     plt.grid()
-    plt.plot(x_values, u_values2, label="Numerical solution")
-    #plt.plot(x_values, u_values, label="exact solution")
-    #plt.plot(x_values, b_values,label="b(x)",linestyle='dashed')
+    plt.plot(x_values, u_values, label="Numerical solution")
+    #plt.plot(x_values, u_values2, label="f")
+    #plt.plot(x_values, 1-x_values**2,
+     #        label="u(x)",
+    #         linestyle='dashed')
     plt.xlabel("x")
-    plt.ylabel("u(x)")
+    plt.ylabel("u")
     plt.legend()
-    plt.title("Solution of the 1D Poisson equation")
-   # plt.savefig("/home/zhenyu/SURE2024/7.10/test1.png")
+    plt.title("Penalty method a=1 (|x|<=0.5) a=-3 (0.5<|x|<=1)")
+   # plt.savefig("/home/zhenyu/SURE2024/plt.plot(x_values, u_values, label="Numerical solution")7.10/test1.png")
     plt.show()
+
+print(uh.x.array[20])
